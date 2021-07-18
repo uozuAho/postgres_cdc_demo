12baa66332efdeaff92a8a01069b5619c426667e
Warwick Stone <warwick_stone@fastmail.com>
2021-07-18 17:37:21 +1000

    consumer: refactor: extract getKinesisRecords

--- kinesis_consumer/consumer.js
+++ kinesis_consumer/consumer.js
@@ -18,7 +18,7 @@ async function sleep(ms) {
  * Get the latest shard iterator from Kinesis
  * @returns {string} shard iterator value
  */
-async function retrieveLatestShardIterator() {
+async function getLatestShardIterator() {
   const command = new GetShardIteratorCommand({
     StreamName: 'Foo',
     ShardId: 'shardId-000000000000',
@@ -28,20 +28,24 @@ async function retrieveLatestShardIterator() {
   return response.ShardIterator;
 }
 
+async function getKinesisRecords() {
+  const command = new GetRecordsCommand({
+    ShardIterator: shardIterator
+  });
+  const response = await kinesisClient.send(command);
+  for (const record of response.Records) {
+    console.log("read record!");
+    console.log(record);
+    shardIterator = response.NextShardIterator;
+  }
+}
+
 async function runConsumerLoop() {
   while (true) {
     if (!shardIterator) {
-      shardIterator = await retrieveLatestShardIterator();
+      shardIterator = await getLatestShardIterator();
     }
-    const command = new GetRecordsCommand({
-      ShardIterator: shardIterator
-    });
-    const response = await kinesisClient.send(command);
-    for (const record of response.Records) {
-      console.log("read record!");
-      console.log(record);
-      shardIterator = response.NextShardIterator;
-    }
+    await getKinesisRecords();
     await sleep(1000);
   }
 }
